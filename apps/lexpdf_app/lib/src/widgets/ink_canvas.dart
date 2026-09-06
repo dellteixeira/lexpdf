368544fd3e6d7f26e880b30518d2f7114eae915b
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-09-06 16:12:15 -0300

    feat: expose pre-mutation hook for notebook history

--- apps/lexpdf_app/lib/src/widgets/ink_canvas.dart
+++ apps/lexpdf_app/lib/src/widgets/ink_canvas.dart
@@ -17,6 +17,7 @@ class InkCanvas extends StatefulWidget {
     this.onStrokeUpdated,
     this.onStrokeErased,
     this.onSelectionChanged,
+    this.onWillMutate,
     this.stylusOnly = true,
     this.eraserMode = false,
     this.lassoMode = false,
@@ -37,6 +38,7 @@ class InkCanvas extends StatefulWidget {
   final ValueChanged<InkStroke>? onStrokeUpdated;
   final ValueChanged<InkStroke>? onStrokeErased;
   final ValueChanged<Set<String>>? onSelectionChanged;
+  final VoidCallback? onWillMutate;
 
   @override
   State<InkCanvas> createState() => InkCanvasState();
@@ -441,6 +443,7 @@ class InkCanvasState extends State<InkCanvas> {
       }
     }
     if (hit == null) return;
+    widget.onWillMutate?.call();
     _strokes.remove(hit);
     _selectedStrokeIds.remove(hit.id);
     widget.onStrokeErased?.call(hit);
@@ -501,6 +504,7 @@ class InkCanvasState extends State<InkCanvas> {
   void _finishStroke(PointerEvent event) {
     _activePoints.add(_pointFromEvent(event));
     if (_activePoints.length >= 2) {
+      widget.onWillMutate?.call();
       final now = DateTime.now().toUtc();
       final stroke = InkStroke(
         id: now.microsecondsSinceEpoch.toRadixString(36),
