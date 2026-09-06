87f3b7bd2569f17d70f3aacad1fbc154bac5b569
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-09-06 13:57:23 -0300

    feat: wire ink store into app

--- apps/lexpdf_app/lib/src/lexpdf_app.dart
+++ apps/lexpdf_app/lib/src/lexpdf_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'core/storage/local_database.dart';
 import 'core/storage/local_document_catalog.dart';
+import 'core/storage/local_ink_store.dart';
 import 'core/storage/local_reading_progress_store.dart';
 import 'core/storage/local_text_annotation_store.dart';
 import 'screens/library_screen.dart';
@@ -19,12 +20,12 @@ class LexPdfApp extends StatefulWidget {
 }
 
 class _LexPdfAppState extends State<LexPdfApp> {
-  late final LocalDocumentCatalog _catalog =
-      LocalDocumentCatalog(widget.database);
+  late final LocalDocumentCatalog _catalog = LocalDocumentCatalog(widget.database);
   late final LocalReadingProgressStore _readingProgress =
       LocalReadingProgressStore(widget.database);
   late final LocalTextAnnotationStore _annotations =
       LocalTextAnnotationStore(widget.database);
+  late final LocalInkStore _inkStore = LocalInkStore(widget.database);
 
   @override
   void dispose() {
@@ -51,6 +52,7 @@ class _LexPdfAppState extends State<LexPdfApp> {
         catalog: _catalog,
         readingProgress: _readingProgress,
         annotations: _annotations,
+        inkStore: _inkStore,
       ),
     );
   }
