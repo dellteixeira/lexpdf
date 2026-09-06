0e547de52647153c82ee165910bc88fd0da77ff7
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-09-06 13:17:12 -0300

    feat: initialize local PDFium engine

--- apps/lexpdf_app/lib/main.dart
+++ apps/lexpdf_app/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pdfrx/pdfrx.dart';
 
 import 'src/lexpdf_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  pdfrxFlutterInitialize();
   runApp(const LexPdfApp());
 }
